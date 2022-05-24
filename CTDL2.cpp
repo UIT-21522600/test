324032cf057ecc01c2c7cd0bcbe915a3a6d67f50
your name <abc@gmail.com>
2022-05-24 10:29:32 +0700

    test

---
+++ CTDL2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct stack{
+    int top;
+    int data[100];
+};
+void SetupStack(stack &s){
+    s.top=-1;
+}
+bool isEmpty(stack s){
+    if (s.top==-1) return true;
+    return false;
+}
+bool isFull (stack s){
+    if (s.top>=100) return true;
+    return false;
+}
+int top (stack s){
+    return s.data[s.top];
+}
+bool pop (stack &s){
+    if (s.top==-1) return 0;
+    s.top--;
+    return 1;
+}
+bool push (stack &s, int _data){
+    if (isFull(s) == false ){
+        s.top++;
+        s.data[s.top] = _data;
+        return 1;
+    } return 0;
+}
+int main(){
+    stack s;
+    SetupStack(s);
+    
+    cout<<top(s);
+    pop(s);
+    cout<<top(s);
+    return 0;
+}
